1bd230255a34ee6d56bce548c5cd0d231cab9ea8
heart <7362469@qq.com>
2022-05-10 15:02:29 +0800

    perf: update 404 pages

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routers'
-
+import './assets/theme/theme.less'
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
--- src/pages/404/index.vue
+++ src/pages/404/index.vue
@@ -9,41 +9,62 @@ function goBack() {
 <template>
   <div class="not-found">
     <div>
-      <div>
-        <span class="goBack" @click="goBack">go back</span>
-        <span>pages not found</span>
-      </div>
       <img src="../../assets/images/404.png" />
+      <div>
+        <span class="not-tips">404</span>
+        <p class="font-24">page not found</p>
+        <div>
+          <button class="goBack btn" @click="goBack">go back</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<style scoped>
+<style lang="less" scoped>
 .not-found {
   width: 100%;
   height: 100%;
   display: flex;
+  font-size: 18px;
 }
 .not-found > div {
   margin: auto;
   display: flex;
   text-align: center;
-  box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.2);
   padding: 20px;
   border-radius: 10px;
 }
 .not-found > div > div {
+  flex-shrink: 0;
   margin: 0 100px;
   display: flex;
   flex-direction: column;
   justify-content: center;
+  & > p {
+    flex-shrink: inherit;
+    margin-bottom: 18px;
+  }
 }
 .not-found img {
   margin: auto;
 }
 .goBack {
-  color: rgb(47 200 149 / 80%);
-  margin: 12px 0;
+  font-size: 16px;
   cursor: pointer;
+  background-color: var(--btn-primary-color);
+  color: var(--color);
+  transition: all 0.3s cubic-bezier(0.19, 1, 0.22, 1);
+  &:hover {
+    background-color: var(--btn-primary-hover-color);
+  }
+}
+.not-tips {
+  font-size: 50px;
+  font-weight: bold;
+  color: var(--btn-primary-color);
+}
+.font-24 {
+  font-size: 24px;
 }
 </style>
